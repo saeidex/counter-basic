a3cc310b6b14755068dd4a6cd90acafceae355bd
saeidex <ebrahim.khalil.nayeem@gmail.com>
2023-06-24 12:03:06 +0600

    added: javascript code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import "./tailwind.css";
+
+let countDisplay = document.getElementById("count-display");
+let btnReset = document.getElementById("btn-reset");
+let btnIncrement = document.getElementById("btn-increment");
+
+let count = 0;
+function increment() {
+  count++;
+  countDisplay.innerText = count;
+}
+function reset() {
+  count = 0;
+  countDisplay.innerText = count;
+}
+
+btnIncrement.addEventListener("click", increment);
+btnReset.addEventListener("click", reset);
